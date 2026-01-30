ac1bfd127ad82dbda753574f19b4c57a2394ef82
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-01-30 23:09:00 +0100

    feat: enable CORS for blog frontend app

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 require('dotenv').config()
 
+const cors = require('cors')
 
 const server = express()
 
 // configurations
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json())
+
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+}
+server.use(cors(corsOptions))
 
 
 // import routes
